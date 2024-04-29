b861bd1770254777c4498346c078780169a3d033
Do Nhat <dmnhat.pt@gmail.com>
2024-04-29 12:58:19 +0700

    Done login fragment.

--- Frontend/app/src/main/java/com/donhat/se330/flower_shop_management/frontend/features/auth/activities/AuthActivity.java
+++ Frontend/app/src/main/java/com/donhat/se330/flower_shop_management/frontend/features/auth/activities/AuthActivity.java
@@ -7,10 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.donhat.se330.flower_shop_management.frontend.R;
+import com.donhat.se330.flower_shop_management.frontend.databinding.ActivityAuthBinding;
+import com.donhat.se330.flower_shop_management.frontend.features.auth.fragments.LoginFragment;
 
 public class AuthActivity extends AppCompatActivity {
+    private ActivityAuthBinding _activityAuthBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +27,16 @@ public class AuthActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        _activityAuthBinding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_auth
+        );
+
+        // Setting fragment transaction
+        final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.auth_fragment, new LoginFragment());
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
