1de9ce0f55c5f24a907041beaf676b98c446b524
JustVvu <21521682@ms.uit.edu.vn>
2024-06-13 15:49:35 +0700

    Final commit

--- Frontend/app/src/main/java/com/donhat/se330/flower_shop_management/frontend/features/customer/category/fragments/CategoryFragment.java
+++ Frontend/app/src/main/java/com/donhat/se330/flower_shop_management/frontend/features/customer/category/fragments/CategoryFragment.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
